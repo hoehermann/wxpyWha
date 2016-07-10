cd1b8c66efb4470f60c369cb6ed4ad7d646c04be
Hermann <hoehermann@gmx.de>
2016-07-10 11:17:28 +0200

    Added generated wxWha.py to git.

---
+++ wxWha.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*- 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Jun 17 2015)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO "NOT" EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class ConversationFrame
+###########################################################################
+
+class ConversationFrame ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.ConversationTextControl = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE )
+		bSizer1.Add( self.ConversationTextControl, 2, wx.ALL|wx.EXPAND, 5 )
+		
+		bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.MessageTextControl = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE )
+		bSizer2.Add( self.MessageTextControl, 5, wx.ALL|wx.EXPAND, 5 )
+		
+		self.SendButton = wx.Button( self, wx.ID_ANY, u"Send", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer2.Add( self.SendButton, 1, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		bSizer1.Add( bSizer2, 1, wx.EXPAND, 5 )
+		
+		
+		self.SetSizer( bSizer1 )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.Bind( wx.EVT_CLOSE, self.onClose )
+		self.SendButton.Bind( wx.EVT_BUTTON, self.onSendButtonClick )
+	
+	def __del__( self ):
+		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def onClose( self, event ):
+		event.Skip()
+	
+	def onSendButtonClick( self, event ):
+		event.Skip()
+	
+
+###########################################################################
+## Class ConversationListFrame
+###########################################################################
+
+class ConversationListFrame ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Conversations", pos = wx.DefaultPosition, size = wx.Size( 269,459 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		bSizer3 = wx.BoxSizer( wx.VERTICAL )
+		
+		ConversationListBoxChoices = []
+		self.ConversationListBox = wx.ListBox( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, ConversationListBoxChoices, wx.LB_SINGLE )
+		bSizer3.Add( self.ConversationListBox, 1, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		self.SetSizer( bSizer3 )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.ConversationListBox.Bind( wx.EVT_LISTBOX, self.onListBox )
+	
+	def __del__( self ):
+		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def onListBox( self, event ):
+		event.Skip()
+	
+
